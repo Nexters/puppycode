2f9c9d324b210750f8a849cf189796829fe10326
sbyeol3 <dev.sbyeol@gmail.com>
2024-07-26 22:44:21 +0900

    Implement Title on setting name page

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -1,23 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:puppycode/pages/setting.dart';
-import 'package:get/get.dart';
+//import 'package:get/get.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
+    return  Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 25, vertical:  30),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [NameGuide(), TextField(
+              decoration: InputDecoration(
+                  hintText: '개떡이',
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                ),
+                ),),],
+        ),
       ),
-      body: Center(
-          child: TextButton(
-        child: const Text('go to setting'),
-        onPressed: () {
-          Get.to(() => SettingPage());
-        },
-      )),
+      bottomSheet: Container(
+        margin: const EdgeInsets.fromLTRB(0,0,0,50),
+        child: Row(
+          children: [
+            ElevatedButton(child: Text('안할래요'), onPressed: () => {},),
+            TextButton(child: Text('등록하기'), onPressed: () => {},)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NameGuide extends StatelessWidget {
+  const NameGuide({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(0, 0, 0, 64),
+      child: const Column(
+        children: [
+          Text(
+          '우리집 강아지 이름을\n설정해볼까요?',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+          ),
+          Text('키우는 강아지가 없어도 괜찮아요', style: TextStyle(fontWeight: FontWeight.w500, color: Colors.grey, fontSize: 16))],
+      ),
     );
   }
 }
