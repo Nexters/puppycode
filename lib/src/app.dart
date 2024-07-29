b9e8b6c19203755766d17934b8be8fe3660e058f
sbyeol3 <dev.sbyeol@gmail.com>
2024-07-29 13:25:51 +0900

    Add theme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,9 +3,15 @@ import 'package:get/get.dart';
 import 'package:puppycode/pages/route.dart';
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.white)
+      ),
       initialRoute: '/',
       getPages: AppRoutes.routes,
     );
