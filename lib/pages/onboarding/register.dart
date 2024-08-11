8933d22a3119f240c57009bbb0a95dfba726b21c
sbyeol3 <dev.sbyeol@gmail.com>
2024-08-12 00:48:39 +0900

    Move Location Widget to register

--- lib/pages/onboarding/register.dart
+++ lib/pages/onboarding/register.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:puppycode/pages/onboarding/start.dart';
 import 'package:puppycode/shared/input.dart';
 import 'package:puppycode/shared/styles/button.dart';
 import 'package:get/get.dart';
+import 'package:puppycode/shared/styles/color.dart';
 import 'package:puppycode/shared/typography/body.dart';
 import 'package:puppycode/shared/typography/head.dart';
 
@@ -34,10 +36,7 @@ class _RegistrationPageState extends State<RegistrationPage> {
               maxLength: 10,
             ),
             const SizedBox(height: 16),
-            TextInputWithBottomSheet(
-              hintText: '지역',
-              onTap: () => {},
-            ),
+            const LocationInputWithBottomSheet(),
             // 지역 INPUT
           ],
         ),
@@ -77,6 +76,142 @@ class GuideText extends StatelessWidget {
             padding: EdgeInsets.only(top: 8),
             child: Body2(value: '키우는 강아지가 없어도 괜찮아요.', color: Color(0xFF50555C)),
           )
+        ],
+      ),
+    );
+  }
+}
+
+List<String> _kLocationValues = [
+  '서울',
+  '부산',
+  '대구',
+  '인천',
+  '광주',
+  '대전',
+  '울산',
+  '경기'
+];
+
+class LocationInputWithBottomSheet extends StatefulWidget {
+  final String? value;
+
+  const LocationInputWithBottomSheet({
+    super.key,
+    this.value,
+  });
+
+  @override
+  State<LocationInputWithBottomSheet> createState() =>
+      _LocationInputWithBottomSheetState();
+}
+
+class _LocationInputWithBottomSheetState
+    extends State<LocationInputWithBottomSheet> {
+  bool _isFocused = false;
+
+  Widget _createLocationSelectContainer(BuildContext context) {
+    return BottomSheet(
+        onClosing: () => {},
+        builder: (context) => Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
+                      topRight: Radius.circular(25))),
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Head3(value: '어디에 사시나요?'),
+                    SizedBox(height: 3),
+                    Body3(value: '지역을 설정하면 날씨 정보를 제공해 드려요.'),
+                    SizedBox(height: 12),
+                    SingleChildScrollView(
+                      child: Column(children: [
+                        for (var location in _kLocationValues)
+                          LocationOption(
+                            location: location,
+                            isSelected: location == '서울',
+                          )
+                      ]),
+                    )
+                  ],
+                ),
+              ),
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () => {
+              setState(() {
+                _isFocused = true;
+              }),
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) =>
+                      _createLocationSelectContainer(context)).whenComplete(() {
+                setState(() {
+                  _isFocused = false;
+                });
+              })
+            },
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(
+                  color: _isFocused ? ThemeColor.primary : ThemeColor.gray2)),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Body1(
+                value: widget.value ?? '지역',
+                color:
+                    widget.value == null ? ThemeColor.gray4 : ThemeColor.gray6,
+              ),
+              RotatedBox(
+                quarterTurns: 3,
+                child: SvgPicture.asset('assets/icons/chevron_left.svg',
+                    colorFilter:
+                        ColorFilter.mode(ThemeColor.gray3, BlendMode.srcIn)),
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class LocationOption extends StatelessWidget {
+  const LocationOption({
+    super.key,
+    required this.location,
+    required this.isSelected,
+  });
+
+  final String location;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 16, 8, 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Body1(value: location),
+          if (isSelected)
+            SvgPicture.asset(
+              'assets/icons/check.svg',
+              colorFilter:
+                  ColorFilter.mode(ThemeColor.primary, BlendMode.srcIn),
+            )
         ],
       ),
     );
