b2e415687714a56367bb5812fead7090ecc9727f
narinn-star <skfls2618@naver.com>
2024-08-12 21:07:02 +0900

    Add ReactionCommentTabView

--- lib/pages/myfeeds/reaction_tab_bar.dart
+++ lib/pages/myfeeds/reaction_tab_bar.dart
@@ -65,19 +65,34 @@ class _ReactionTabBarState extends State<ReactionTabBar>
       body: TabBarView(controller: _tabController, children: const [
         Padding(
           padding: EdgeInsets.symmetric(vertical: 12),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                ReactionEmojiList(emoji: '😆', userName: '푸름이'),
-                ReactionEmojiList(emoji: '😍', userName: '앙꼬'),
-                ReactionEmojiList(emoji: '😍', userName: '샛별이'),
-              ],
+          child: Scrollbar(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  ReactionEmojiListItem(emoji: '😆', userName: '푸름이'),
+                  ReactionEmojiListItem(emoji: '😍', userName: '앙꼬'),
+                  ReactionEmojiListItem(emoji: '😍', userName: '샛별이'),
+                ],
+              ),
             ),
           ),
         ),
-        Center(
-          child: Body2(
-            value: '이모지',
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 12),
+          child: Scrollbar(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  ReactionCommentListItem(
+                      userName: '푸름이', comment: 'ㅋㅋㅋ우리집 강아지도 산책만 들으면 환장을 함'),
+                  ReactionCommentListItem(
+                      userName: '샛별이', comment: '우리집 강아지가 젤 귀여움 ☀︎'),
+                  ReactionCommentListItem(
+                      userName: '앙꼬',
+                      comment: '미쳤다 저정도 정전기라면 모든 것을 이겨낼 수 있지 않을까 캬캬캬캬캬캬캬캬캬캬')
+                ],
+              ),
+            ),
           ),
         ),
       ]),
@@ -85,43 +100,48 @@ class _ReactionTabBarState extends State<ReactionTabBar>
   }
 }
 
-class ReactionEmojiList extends StatelessWidget {
-  final String emoji;
+class ReactionCommentListItem extends StatelessWidget {
+  // final Image profileImage;
   final String userName;
+  final String comment;
 
-  const ReactionEmojiList({
+  const ReactionCommentListItem({
     super.key,
-    required this.emoji,
+    // required this.profileImage,
     required this.userName,
+    required this.comment,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 58,
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 12),
+      width: MediaQuery.of(context).size.width,
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(
-                height: 34,
-                width: 34,
-                decoration: BoxDecoration(
-                    color: ThemeColor.gray2,
-                    borderRadius: BorderRadius.circular(21.25)),
-              ),
-              Text(
-                emoji,
-                style: const TextStyle(
-                    fontSize: 21.25,
-                    letterSpacing: -0.012 * 21.25,
-                    height: 29.8 / 21.25),
-              ),
-            ],
+          Container(
+            width: 42,
+            height: 42,
+            decoration: BoxDecoration(
+              color: ThemeColor.gray2,
+              borderRadius: BorderRadius.circular(100),
+            ),
           ),
           const SizedBox(width: 8),
-          Body1(value: userName),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Body3(value: userName, bold: true),
+              const SizedBox(height: 2),
+              SizedBox(
+                width: MediaQuery.of(context).size.width -
+                    93, // 지정해둔 width를 넘으면 개행시킬 수 있는 방법이 있나요 ..?
+                child: Body3(value: comment),
+              )
+            ],
+          )
         ],
       ),
     );
@@ -163,6 +183,49 @@ class CustomTab extends StatelessWidget {
             Text(label),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ReactionEmojiListItem extends StatelessWidget {
+  final String emoji;
+  final String userName;
+
+  const ReactionEmojiListItem({
+    super.key,
+    required this.emoji,
+    required this.userName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 58,
+      child: Row(
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                height: 34,
+                width: 34,
+                decoration: BoxDecoration(
+                    color: ThemeColor.gray2,
+                    borderRadius: BorderRadius.circular(21.25)),
+              ),
+              Text(
+                emoji,
+                style: const TextStyle(
+                    fontSize: 21.25,
+                    letterSpacing: -0.012 * 21.25,
+                    height: 29.8 / 21.25),
+              ),
+            ],
+          ),
+          const SizedBox(width: 8),
+          Body1(value: userName),
+        ],
       ),
     );
   }
