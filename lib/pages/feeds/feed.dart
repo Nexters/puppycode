c44b451d50c56079ffa15a9e772fd3070e890647
sbyeol3 <dev.sbyeol@gmail.com>
2024-08-13 03:36:49 +0900

    Add focus effect

--- lib/pages/feeds/feed.dart
+++ lib/pages/feeds/feed.dart
@@ -3,23 +3,37 @@ import 'package:flutter/widgets.dart';
 import 'package:puppycode/pages/feeds/feed_friends.dart';
 import 'package:puppycode/pages/feeds/feed_scroll.dart';
 
-class FeedScreen extends StatelessWidget {
+class FeedScreen extends StatefulWidget {
   const FeedScreen({super.key});
 
   @override
+  State<FeedScreen> createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  String? focusedUserId;
+
+  void setFocusUser(String? userId) {
+    setState(() {
+      focusedUserId = focusedUserId == userId ? null : userId;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const FeedFriends(),
-          Expanded(
-            child: Container(
-                padding: const EdgeInsets.fromLTRB(20, 12, 20, 0),
-                child: const FeedListView()),
-          )
-        ],
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        FeedFriends(
+          onSelect: setFocusUser,
+          focusedUserId: focusedUserId,
+        ),
+        Expanded(
+          child: Container(
+              padding: const EdgeInsets.fromLTRB(20, 12, 20, 0),
+              child: const FeedListView()),
+        )
+      ],
     );
   }
 }
