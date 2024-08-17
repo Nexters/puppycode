0dff87f26e124adb7a9ca513bb8c773bab0edff4
sbyeol3 <dev.sbyeol@gmail.com>
2024-08-17 22:15:22 +0900

    Fix appbar height

--- lib/shared/app_bar.dart
+++ lib/shared/app_bar.dart
@@ -80,7 +80,7 @@ class SharedAppBar extends StatelessWidget implements PreferredSizeWidget {
         : '';
     return Positioned(
       left: 0,
-      height: 54,
+      height: 50,
       child: Align(
         alignment: Alignment.centerLeft,
         child: leftOptions!.label != null
@@ -131,7 +131,7 @@ class SharedAppBar extends StatelessWidget implements PreferredSizeWidget {
 // ignore: non_constant_identifier_names
   Widget? Right(BuildContext context) {
     return Positioned(
-      height: 54,
+      height: 50,
       right: 0,
       child: Align(
         alignment: Alignment.centerRight,
@@ -174,7 +174,7 @@ class SharedAppBar extends StatelessWidget implements PreferredSizeWidget {
     return SafeArea(
         child: Container(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      height: 56,
+      height: 50,
       child: Stack(
         children: children,
       ),
