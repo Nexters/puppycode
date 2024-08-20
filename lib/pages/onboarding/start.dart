cbb4a0c2d485803833619f38760cdd7ef9d8b357
sbyeol3 <dev.sbyeol@gmail.com>
2024-08-21 02:48:58 +0900

    Modify start page

--- lib/pages/onboarding/start.dart
+++ lib/pages/onboarding/start.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:puppycode/shared/typography/head.dart';
 
 class StartPage extends StatelessWidget {
   const StartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    String name = '';
+    if (Get.arguments != null) {
+      name = Get.arguments['name'] ?? '';
+    }
     return Scaffold(
       body: Center(
           child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          const Text(
-            '개떡이와 오늘도\n산책하러 갈까요?',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.black, fontSize: 24, fontWeight: FontWeight.w600),
-          ),
-          TextButton(
-              onPressed: () => {Get.toNamed('/home')},
-              child: const Text('피드로 ㄱㄱ'))
+          Head1(value: name.isNotEmpty ? '$name와 오늘도\n산책하러 갈까요?' : '산책하러 갈까요?'),
+          GestureDetector(
+            child: Padding(
+                padding: const EdgeInsets.all(90),
+                child: Image.asset('assets/images/pawpaw_hello.png')),
+          )
         ],
       )),
     );
